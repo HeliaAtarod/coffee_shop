f88587a810eb7b19b5fc94e6fcf77923f58495d6
Ali Mahdian <a.mahdian@digikala.com>
2024-06-27 15:57:30 +0330

    Profile, OTP login and verification views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from api.views import CreateUserView
+from api.views import CreateUserView, ProfileView, OTPLoginView, OTPVerificationView
 
 urlpatterns = [
     path('user/', CreateUserView.as_view(), name='create-user'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('otp-login/', OTPLoginView.as_view(), name='otp-login'),
+    path('otp-verify/', OTPVerificationView.as_view(), name='otp-verify'),
 ]
