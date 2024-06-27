aa78beaab359c9d559bf7f8c58749cf48b2a0e6e
Ali Mahdian <a.mahdian@digikala.com>
2024-06-27 15:58:28 +0330

    use jwt views for authentication

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api.views import CreateUserView, ProfileView, OTPLoginView, OTPVerificationView
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('otp-login/', OTPLoginView.as_view(), name='otp-login'),
     path('otp-verify/', OTPVerificationView.as_view(), name='otp-verify'),
+    path('token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
